1f7a80075ab6900dcfe1288c7934582f19650547
some-earth11 <sk8762@srmist.edu.in>
2022-09-24 14:17:14 +0530

    added Images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+const cardImages = [
+  { "src" : "/img/helmet-1.png" },
+  { "src" : "/img/potion-1.png" },
+  { "src" : "/img/ring-1.png" },
+  { "src" : "/img/scroll-1.png" },
+  { "src" : "/img/shield-1.png" },
+  { "src" : "/img/sword-1.png" }
+]
 
 function App() {
   return (
